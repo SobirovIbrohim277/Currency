620f43a352d3852ae3d21b4c2bffb2e1a63af049
Sobirovibrohim277 <i.coder.prog@gmail.com>
2021-10-19 20:04:40 +0500

    exam

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:imtihon3/ui/loginpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Imtihon3(),
+      
+    );
+  }
+}
